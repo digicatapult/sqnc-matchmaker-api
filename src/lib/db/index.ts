f56c47d1e0d5de04b697909d5f339680eaeecd64
Paulius <paulius.michelevicius@gmail.com>
2023-03-13 15:56:54 +0000

    L3-25: updating model types.

--- src/lib/db/index.ts
+++ src/lib/db/index.ts
@@ -8,6 +8,10 @@ import { pgConfig } from './knexfile'
 
 const MODELS_DIRECTORY = path.join(__dirname, '../../models')
 
+interface Models<V> {
+  [key: string]: V[keyof V]
+}
+
 /** Creates a connection to the postgres instance
  * usage: var db = new Database().init()
  * db.Example().where(id); db.Table.select(id);
@@ -15,13 +19,13 @@ const MODELS_DIRECTORY = path.join(__dirname, '../../models')
 export default class Database {
   public client: Knex
   private log: Logger
-  public db: any
+  public db: Models<Knex.QueryBuilder> | any // TODO
 
   constructor() {
     this.log = logger
     this.client = knex(pgConfig)
-    this.db = (models: any = {}) => {
-      fs.readdirSync(MODELS_DIRECTORY).forEach((file: string): any => {
+    this.db = (models: Models<Knex.QueryBuilder> = {}) => {
+      fs.readdirSync(MODELS_DIRECTORY).forEach((file: string) => {
         this.log.debug('initializing db models')
         const { name } = path.parse(file)
         // TODO check if table exists -> append to the db object
