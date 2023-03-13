80648473d5db15103f1a49f7d866b52ab34f9ccf
Paulius <paulius.michelevicius@gmail.com>
2023-03-13 13:35:00 +0000

    L3-29: version bump and linting.

--- src/lib/db/migrations/20230313130941_attachments-table.ts
+++ src/lib/db/migrations/20230313130941_attachments-table.ts
@@ -1,18 +1,20 @@
-import { Knex } from "knex";
+import { Knex } from 'knex'
 
 export async function up(knex: Knex): Promise<void> {
-    await knex.schema.createTable('attachments', (def) => {
-        def.uuid('id').defaultTo(knex.raw('uuid_generate_v4()'))
-        def.string('filename', 255).notNullable()
-        def.binary('binary_blob').notNullable()
-        def.datetime('created_at').notNullable().defaultTo(knex.fn.now())
-    
-        def.primary(['id'])
-      })
-}
+  const [extInstalled] = await knex('pg_extension').select('*').where({ extname: 'uuid-ossp' })
 
+  if (!extInstalled) await knex.raw('CREATE EXTENSION "uuid-ossp"')
+
+  await knex.schema.createTable('attachments', (def) => {
+    def.uuid('id').defaultTo(knex.raw('uuid_generate_v4()'))
+    def.string('filename', 255).notNullable()
+    def.binary('binary_blob').notNullable()
+    def.datetime('created_at').notNullable().defaultTo(knex.fn.now())
+
+    def.primary(['id'])
+  })
+}
 
 export async function down(knex: Knex): Promise<void> {
-    await knex.schema.dropTable('attachments')
+  await knex.schema.dropTable('attachments')
 }
-
