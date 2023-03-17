ab416e0a3567581b9f492487ed76ba8b8855f22a
Paulius <paulius.michelevicius@gmail.com>
2023-03-17 14:29:24 +0000

    L3-25: refining not found error handler.

--- src/lib/services/identity.ts
+++ src/lib/services/identity.ts
@@ -12,7 +12,7 @@ const getMemberByAlias = async (alias: string) => {
   }
 
   if (res.status === 404) {
-    throw new NotFound('identity')
+    throw new NotFound(`identity: ${alias}`)
   }
 
   throw new HttpResponse({})
