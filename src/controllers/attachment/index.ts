eb6da75389e8baba40affe42ab038b00729f6d4b
Paulius <paulius.michelevicius@gmail.com>
2023-03-21 11:47:01 +0000

    L3-25: if JSON parse throws return octet.

--- src/controllers/attachment/index.ts
+++ src/controllers/attachment/index.ts
@@ -24,6 +24,8 @@ type File = {
   [k: string]: string
 }
 
+
+
 @Route('attachment')
 @Tags('attachment')
 @Security('bearerAuth')
@@ -37,6 +39,17 @@ export class attachment extends Controller {
     this.log = logger.child({ controller: '/attachment' })
     // TMP will be updated with a wrapper so ORM client independant
     this.db = this.dbClient.db()
+  }
+
+  returnOctet(blob: Blob, name: string) {
+    // default to octect-stream or allow error middleware to handle
+    this.setHeader('accept', 'application/octet-stream')
+    this.setHeader('access-control-expose-headers', 'content-disposition')
+    this.setHeader('content-disposition', `attachment; filename="${name}"`)
+    this.setHeader('maxAge', `${365 * 24 * 60 * 60 * 1000}`)
+    this.setHeader('immutable', 'true')
+
+    return blob
   }
 
   @Get('/')
@@ -53,30 +66,27 @@ export class attachment extends Controller {
 
   @Post('/')
   @SuccessResponse(201, 'attachment has been created')
-  public async create(@Request() req: express.Request, @UploadedFile() file: File): Promise<{ [k: string]: string }> {
+  public async create(@Request() req: express.Request, @UploadedFile() file: File): Promise<Attachment> {
     this.log.debug(`creating an attachment ${JSON.stringify(file || req.body)}`)
 
     if (file)
-      return {
-        attachment: await this.db.attachment('id').insert({
+      return await this.db
+        .attachment('id')
+        .insert({
           filename: file.originalname,
           binary_blob: Buffer.from(file.buffer),
-        }),
-        message: 'binary attachment has been created',
-      }
+        })
+        .returning(['id', 'filename', 'binary_blob', 'datetime'])
 
     if (!req.body) throw new BadRequest('nothing to upload')
 
-    return {
-      attachment: await this.db
-        .attachment()
-        .insert({
-          filename: 'json',
-          binary_blob: Buffer.from(JSON.stringify(req.body)),
-        })
-        .then((row: Array<unknown>) => row[0]), // row[0] - is first column in this case id
-      message: 'JSON attachment has been created',
-    }
+    return await this.db
+      .attachment()
+      .insert({
+        filename: 'json',
+        binary_blob: Buffer.from(JSON.stringify(req.body)),
+      })
+      .returning(['id', 'filename', 'binary_blob', 'datetime'])
   }
 
   @Get('/{id}')
@@ -87,25 +97,23 @@ export class attachment extends Controller {
     @Request() req: express.Request,
     @Path() id: string,
     @Header('return-type') type: 'json' | 'file'
-  ): Promise<Attachment> {
+  ): Promise<Attachment | Blob> {
     this.log.debug(`attempting to retrieve ${id} attachment`)
     const { accept } = req.headers
     const [attachment] = await this.db.attachment().where({ id })
     if (!attachment) throw new NotFound('attachment')
     const { filename, binary_blob } = attachment
 
+    // log and default to octect-stream  
     if (type === 'json' || accept === 'application/json') {
       this.setHeader('content-type', 'application/json')
-      return JSON.parse(binary_blob)
+      try {
+        return JSON.parse(binary_blob)
+      } catch(err) {
+        this.log.warn('requested type failed, returning as octet')
+      }
     }
 
-    // default to octect-stream or allow error middleware to handle
-    this.setHeader('accept', 'application/octet-stream')
-    this.setHeader('access-control-expose-headers', 'content-disposition')
-    this.setHeader('content-disposition', `attachment; filename="${filename}"`)
-    this.setHeader('maxAge', `${365 * 24 * 60 * 60 * 1000}`)
-    this.setHeader('immutable', 'true')
-
-    return binary_blob
+    return this.returnOctet(binary_blob, filename)
   }
 }
