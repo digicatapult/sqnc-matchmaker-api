d78806768d045dec9e6ed18becb2342e01518aac
Mark Addis <35331926+nitro-marky@users.noreply.github.com>
2023-04-18 14:25:29 +0100

    Lint and test body response

--- test/integration/offchain/attachment.test.ts
+++ test/integration/offchain/attachment.test.ts
@@ -127,8 +127,9 @@ describe('attachment', async () => {
 
     it('Doesn`t upload files if more than 100mb', async () => {
       const uploadRes = await postFile(app, '/attachment', Buffer.from(overSizeBlobData), 'json')
-      const { status  } = await get(app, `/attachment/${uploadRes.body.id}`)
-    
+      const { status, body  } = await get(app, `/attachment/${uploadRes.body.id}`)
+      console.log(Buffer.from(body).toString())
       expect(status).to.equal(422)
+      expect(Buffer.from(body).toString()).to.not.equal("Unexpected error thrown in handler: File too large")
     })
 })
