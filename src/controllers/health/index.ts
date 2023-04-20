d3879c8cf77725c778f39c87cac17e404a65c224
Mark Addis <35331926+nitro-marky@users.noreply.github.com>
2023-04-20 15:45:51 +0100

    More changes

--- src/controllers/health/index.ts
+++ src/controllers/health/index.ts
@@ -4,6 +4,10 @@ import { Logger } from 'pino'
 import * as Ex from 'express'
 import type { Health } from '../../models'
 import { logger } from '../../lib/logger'
+import { startStatusHandlers } from '../../lib/ServiceWatcher/index'
+import { serviceState } from '../../lib/util/statusPoll'
+
+
 @Route('health')
 export class health extends Controller {
   constructor() {
@@ -14,9 +18,30 @@ export class health extends Controller {
   public async get(@Request() req: Ex.Request & { log: Logger; req_id: string }): Promise<Health> {
     logger.debug({ msg: 'new request received', controller: '/health' })
 
-    return Promise.resolve({
-      message: 'success',
-      req_id: req.req_id,
-    })
+    const serviceStatusStrings = {
+      [serviceState.UP]: 'ok',
+      [serviceState.DOWN]: 'down',
+      [serviceState.ERROR]: 'error',
+    }
+
+    const statusHandler = await startStatusHandlers()
+    const status = statusHandler.status
+    const details = statusHandler.detail
+    const code = status === serviceState.UP ? 200 : 503
+    return Promise.resolve(
+      res.status(code).send({
+        version: process.env.npm_package_version ? process.env.npm_package_version : 'unknown',
+        status: serviceStatusStrings[status] || 'error',
+        details: Object.fromEntries(
+          Object.entries(details).map(([depName, { status, detail }]) => [
+            depName,
+            {
+              status: serviceStatusStrings[status] || 'error',
+              detail,
+            },
+          ])
+        ),
+      })
+    )
   }
 }
--- src/server.ts
+++ src/server.ts
@@ -6,39 +6,13 @@ import { json, urlencoded } from 'body-parser'
 import { errorHandler } from './lib/error-handler'
 import { RegisterRoutes } from './routes'
 import * as swaggerJson from './swagger.json'
-import { startStatusHandlers } from '../src/lib/ServiceWatcher/index'
-import { serviceState } from './lib/util/statusPoll'
 
 export default async (): Promise<Express> => {
   const app: Express = express()
-  const statusHandler = await startStatusHandlers()
-  const serviceStatusStrings = {
-    [serviceState.UP]: 'ok',
-    [serviceState.DOWN]: 'down',
-    [serviceState.ERROR]: 'error',
-  }
+
   app.use(urlencoded({ extended: true }))
   app.use(json())
   app.use(cors())
-
-  app.get('/health', async (_req, res) => {
-    const status = statusHandler.status
-    const details = statusHandler.detail
-    const code = status === serviceState.UP ? 200 : 503
-    res.status(code).send({
-      version: process.env.npm_package_version ? process.env.npm_package_version : 'unknown',
-      status: serviceStatusStrings[status] || 'error',
-      details: Object.fromEntries(
-        Object.entries(details).map(([depName, { status, detail }]) => [
-          depName,
-          {
-            status: serviceStatusStrings[status] || 'error',
-            detail,
-          },
-        ])
-      ),
-    })
-  })
 
   RegisterRoutes(app)
   app.use(errorHandler)
