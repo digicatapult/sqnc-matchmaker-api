6b7121209d479ef9dc3c1e04c9c467ffcc3606b3
Matthew Dean <matthew.dean@digicatapult.org.uk>
2023-04-26 10:05:30 +0100

    Move indexer init to server

--- src/index.ts
+++ src/index.ts
@@ -3,29 +3,8 @@ import { Express } from 'express'
 import Server from './server'
 import env from './env'
 import { logger } from './lib/logger'
-
-import Database from './lib/db'
-import Indexer from './lib/indexer'
-import ChainNode from './lib/chainNode'
 ;(async () => {
   const app: Express = await Server()
-
-  const node = new ChainNode({
-    host: env.NODE_HOST,
-    port: env.NODE_PORT,
-    logger,
-    userUri: env.USER_URI,
-  })
-
-  if (env.ENABLE_INDEXER) {
-    const indexer = new Indexer({ db: new Database(), logger, node })
-    await indexer.start()
-    indexer.processAllBlocks(await node.getLastFinalisedBlockHash()).then(() =>
-      node.watchFinalisedBlocks(async (hash) => {
-        await indexer.processAllBlocks(hash)
-      })
-    )
-  }
 
   app.listen(env.PORT, () => {
     logger.info(`dscp-matchmaker-api listening on ${env.PORT} port`)
