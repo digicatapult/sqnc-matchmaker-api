282a8183c0ad084a77d223d1c0f13e6d64874ed6
Paulius Michelevicius <paulius.michelevicius@gmail.com>
2023-09-14 14:40:44 +0100

    corrected cancellation -> cancelled

--- src/lib/db/migrations/20230914131825_math2-cancellation.ts
+++ src/lib/db/migrations/20230914131825_math2-cancellation.ts
@@ -2,10 +2,10 @@ import { Knex } from 'knex'
 
 export const up = async (knex: Knex): Promise<void> => {
   await knex.raw('ALTER TYPE "transaction_type" ADD VALUE \'cancellation\'')
-  await knex.raw('ALTER TYPE "match2_state" ADD VALUE \'cancellation\'')
+  await knex.raw('ALTER TYPE "match2_state" ADD VALUE \'cancelled\'')
 }
 
 export const down = async (knex: Knex): Promise<void> => {
   await knex.raw('ALTER TYPE "transaction_type" REMOVE VALUE \'cancellation\'')
-  await knex.raw('ALTER TYPE "match2_state" REMOVE VALUE \'cancellation\'')
+  await knex.raw('ALTER TYPE "match2_state" REMOVE VALUE \'cancelled\'')
 }
