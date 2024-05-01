ebbb87f014b48aabac1eaa4b7d85118c3c3fc19b
Paulius Michelevicius <pmichelevicius@F4QVY4N44X.local>
2024-05-01 11:17:27 +0100

    L3-NULL: a few minor changes to the middleware.,

--- src/lib/authentication.ts
+++ src/lib/authentication.ts
@@ -1,41 +1,40 @@
 import * as express from 'express'
 import * as jwt from 'jsonwebtoken'
-import jwksRsa from 'jwks-rsa';
+import jwksRsa from 'jwks-rsa'
 
 type _Obj = { [k: string]: string }
 
 /* eslint-disable @typescript-eslint/no-unused-vars */
-export function checkJwks(
+export function expressAuthentication(
   request: express.Request,
-  securityName: string,
+  securityName: string
   // scopes?: string[]
 ): any {
   if (securityName === 'auth0') {
-    const token = request.headers['authorization'];
+    const token = request.headers['authorization']
     if (!token) {
       throw new Error('No token provided')
     }
 
     const client: jwksRsa.JwksClient = jwksRsa({
       cache: true,
-      rateLimit: true,
-      jwksRequestsPerMinute: 5,
-      jwksUri: 'https://<TODO>.eu.auth0.com/.well-known/jwks.json'
-    });
+      jwksUri: 'https://<TODO>.eu.auth0.com/.well-known/jwks.json',
+    })
 
     const getKey = (header: any, callback: any) => {
-      client.getSigningKey(header.kid, function(err: any, key: any) {
-        var signingKey = key.publicKey || key.rsaPublicKey;
-        callback(null, signingKey);
-      });
-    }
-
-      jwt.verify(token, getKey, function(err: any, decoded: any) {
-        if (err) throw new Error(err.message)
-        console.log({ decoded })
-
-        return true
+      client.getSigningKey(header.kid, function (err: any, key: any) {
+        const signingKey = key.publicKey || key.rsaPublicKey
+        callback(null, signingKey)
       })
     }
-  return Promise.reject('error validating token')
+
+    jwt.verify(token, getKey, function (err: any, decoded: any) {
+      if (err) throw new Error(err.message)
+      console.log({ decoded })
+
+      return decoded
+    })
+  }
+
+  throw new Error('403 - error validating token')
 }
