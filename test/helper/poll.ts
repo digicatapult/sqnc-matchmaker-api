b6be64156474445b996c687db388faad64c60475
Matthew Dean <matthew.dean@digicatapult.org.uk>
2024-11-21 15:10:57 +0000

    Fix linting issues

--- test/helper/poll.ts
+++ test/helper/poll.ts
@@ -1,24 +1,44 @@
 import Database from '../../src/lib/db/index.js'
-import { TransactionResponse } from '../../src/models/transaction.js'
 import { UUID } from '../../src/models/strings.js'
 
 type Method = 'getTransaction' | 'getDemand' | 'getDemandCommentForTransaction' | 'getMatch2' extends keyof Database
   ? 'getTransaction' | 'getDemand' | 'getDemandCommentForTransaction' | 'getMatch2'
   : never
 
+const getRow = async (db: Database, method: Method, id: string): Promise<{ state: string } | undefined> => {
+  switch (method) {
+    case 'getTransaction': {
+      const [row] = await db.getTransaction(id)
+      return row
+    }
+    case 'getDemand': {
+      const [row] = await db.getDemand(id)
+      return row
+    }
+    case 'getDemandCommentForTransaction': {
+      const [row] = await db.getDemandCommentForTransaction(id)
+      return row
+    }
+    case 'getMatch2': {
+      const [row] = await db.getMatch2(id)
+      return row
+    }
+  }
+}
+
 const pollState =
   (method: Method) =>
   async (db: Database, id: UUID | undefined, targetState: string, delay = 100, maxRetry = 100): Promise<void> => {
     let retry = 0
 
-    const poll = async (): Promise<TransactionResponse> => {
+    const poll = async (): Promise<void> => {
       if (retry >= maxRetry) {
         throw new Error(`Maximum number of retries exceeded while waiting for  ${id} to reach state ${targetState}`)
       }
 
-      const [row] = await db[method](id)
+      const row = await getRow(db, method, id || '')
       if (row && row.state === targetState) {
-        return row
+        return
       }
 
       retry += 1
