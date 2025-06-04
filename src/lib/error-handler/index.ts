88725cb83b8b388d45b40a87a0e43e2ae2e0ff8d
Jonathan Gray <jonmattgray@gmail.com>
2025-06-04 09:15:18 +0100

    Update to Express v5 (#713)
    * express v5
    
    * Updating version to 6.2.44
    
    * returns
    
    ---------
    
    Co-authored-by: dc-autobot[bot] <181364585+dc-autobot[bot]@users.noreply.github.com>

--- src/lib/error-handler/index.ts
+++ src/lib/error-handler/index.ts
@@ -82,36 +82,41 @@ export const errorHandler = function errorHandler(
   req: ExRequest,
   res: ExResponse,
   next: NextFunction
-): ExResponse | void {
+): void {
   const logger = container.resolve<Logger>(LoggerToken)
   if (err instanceof OauthError || err instanceof AggregateOAuthError) {
-    return res.status(401).send({
+    res.status(401).send({
       message: err.message,
     })
+    return
   }
   if (err instanceof ValidateError) {
     logger.warn(`Handled Validation Error for ${req.path}: %s`, JSON.stringify(err.fields))
 
     const { status, ...rest } = err
 
-    return res.status(422).send({
+    res.status(422).send({
       ...rest,
       message: 'Validation failed',
     })
+    return
   }
   if (err instanceof ServiceUnavailable) {
     logger.warn('Error thrown in Health Watcher')
-    return res.status(err.code).json(err.data)
+    res.status(err.code).json(err.data)
+    return
   }
   if (err instanceof HttpResponse) {
     logger.warn('Error thrown in handler: %s', err.message)
 
-    return res.status(err.code).json(err.message)
+    res.status(err.code).json(err.message)
+    return
   }
   if (err instanceof Error) {
     logger.error('Unexpected error thrown in handler: %s', err.message)
 
-    return res.status(500).json(err)
+    res.status(500).json(err)
+    return
   }
 
   next()
